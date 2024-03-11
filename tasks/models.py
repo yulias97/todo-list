2609fe4197f0c996ebcfe11fc3e3fb5b6f5c1e3c
yulias97 <yulkastetsko@gmail.com>
2024-03-11 11:26:49 +0200

    create models and specify related settings

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tags")
+
+    class Meta:
+        ordering = ["is_done", "-datetime"]
+
+    def __str__(self):
+        return self.content
